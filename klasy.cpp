985572b7da4e0f717336d94996ca5f12676c25e7
Krzysztof Bicz <kb305344@student.polsl.pl>
2023-12-09 20:38:26 +0100

    Klasy funkcji wraz z definicjami

--- klasy.cpp
+++ klasy.cpp
@@ -1,4 +1,5 @@
 #include "klasy.h"
+#include <cmath>
 
 void Funkcje::setX(double n_x)
 {
@@ -25,7 +26,7 @@ void liniowa::setA(double n_a)
     this->a = n_a;
 }
 
-double liniowa::setB(double n_b)
+void liniowa::setB(double n_b)
 {
     this->b = n_b;
 }
@@ -40,9 +41,135 @@ double liniowa::getB()
     return this->b;
 }
 
+double liniowa::obliczY()
+{
+    double y;
+
+    y = a * getX() + b;
+
+    return y;
+}
+
 liniowa::liniowa(double x, double a, double b)
 {
     setX(x);
     setA(a);
     setB(b);
+}
+
+void logarytmiczna::setA(double n_a)
+{
+    this->a = n_a;
+}
+
+void logarytmiczna::setB(double n_b)
+{
+    this->b = n_b;
+}
+
+void logarytmiczna::setC(double n_c)
+{
+    this->c = n_c;
+}
+
+double logarytmiczna::getA()
+{
+    return this->a;
+}
+
+double logarytmiczna::getB()
+{
+    return this->b;
+}
+
+double logarytmiczna::getC()
+{
+    return this->c;
+}
+
+double logarytmiczna::obliczY()
+{
+    double y;
+
+    y = a * (log10(getX()) / log10(b)) + c;
+
+    return y;
+}
+
+void sinus::setA(double n_a)
+{
+    this->a = n_a;
+}
+
+void sinus::setB(double n_b)
+{
+    this->b = n_b;
+}
+
+void sinus::setF(double n_f)
+{
+    this->f = n_f;
+}
+
+void sinus::setFi(double n_fi)
+{
+    this->fi = n_fi;
+}
+
+double sinus::getA()
+{
+    return this->a;
+}
+
+double sinus::getB()
+{
+    return this->b;
+}
+
+double sinus::getF()
+{
+    return this->f;
+}
+
+double sinus::getFi()
+{
+    return this->fi;
+}
+
+double sinus::obliczY()
+{
+    double y;
+
+    y = a * sin((2 * 3.14 * getX() * fi)) + b;
+
+    return y;
+}
+
+void pierwiastek::setA(double n_a)
+{
+    this->a = n_a;
+}
+
+void pierwiastek::setD(double n_d)
+{
+    this->d = n_d;
+}
+
+double pierwiastek::getA()
+{
+    return this->a;
+}
+
+double pierwiastek::getD()
+{
+    return this->d;
+}
+
+double pierwiastek::obliczY()
+{
+    double y;
+
+    y = a * sqrt(getX()) + d;
+
+    return y;
 }
--- klasy.h
+++ klasy.h
@@ -34,7 +34,59 @@ public:
 
 class logarytmiczna : public Funkcje
 {
-    
+private:
+    double a;
+    double b;
+    double c;
+
+public:
+    void setA(double n_a);
+    void setB(double n_b);
+    void setC(double n_c);
+
+    double getA();
+    double getB();
+    double getC();
+
+    double obliczY();
+};
+
+class sinus : public Funkcje
+{
+private:
+    double a;
+    double b;
+    double f;
+    double fi;
+
+public:
+    void setA(double n_a);
+    void setB(double n_b);
+    void setF(double n_f);
+    void setFi(double n_fi);
+
+    double getA();
+    double getB();
+    double getF();
+    double getFi();
+
+    double obliczY();
+};
+
+class pierwiastek : public Funkcje
+{
+private:
+    double a;
+    double d;
+
+public:
+    void setA(double n_a);
+    void setD(double n_d);
+
+    double getA();
+    double getD();
+
+    double obliczY();
 };
 
 #endif // KLASY_H
